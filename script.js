05146b3b3fe68ecbe08ae25732d49f30bd4ad281
James <james83lau@gmail.com>
2019-02-22 10:44:26 -0500

    updated dataType

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 const app = {};
 app.yummlyApiKey = '8e9501ed6ce9a437dced0f55150d990d';
-app.yummlyApiUrl = 'http://api.yummly.com/v1/api/recipes?';
+app.yummlyApiUrl = 'https://api.yummly.com/v1/api/recipes';
 
 $(function () {
   app.init();
@@ -11,16 +10,17 @@ app.init = function(){
   $.ajax({
     url: app.yummlyApiUrl,
     method: 'GET',
-    dataType: 'json',
+    dataType: 'jsonp',
     data: {
       key: app.yummlyApiKey,
       // id: '44d38b57',
-      format: 'json',
+      format: 'jsonp',
     }
   }).then(function(result) {
     console.log(result)
   })
-=======
+}
+
 const movieApp = {};
 
 movieApp.apiKey = '59be6234ea545ba637c135657e114e1d';
@@ -42,5 +42,4 @@ movieApp.getMovies = () => {
     }).then((result) => {
         console.log('test')
     });
->>>>>>> jamesbranch
 }
