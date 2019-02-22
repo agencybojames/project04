379b2af3188ce0b559ec5c055a2e608c96413b4c
Bojan <bojanpecanac89@gmail.com>
2019-02-22 16:56:20 -0500

    2 working api calls and some html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const app = {};
 
 app.yummlyApiKey = '4232b467d64427cc7880acacaa0f27bc';
-app.yummlyApiUrl = 'http://api.yummly.com/v1/api/recipes?';
+app.yummlyApiUrl = 'http://api.yummly.com/v1/api/recipes?'; // http://api.yummly.com/v1/api/metadata/
 app.yummlyApiID = '44d38b57';
 
 app.movieDBKey = '59be6234ea545ba637c135657e114e1d'
-app.movieDBUrl = 'https://api.themoviedb.org/3/search/movie'
+app.movieDBUrl = 'https://api.themoviedb.org/3/discover/movie'
 
   $(function () {
     app.init();
@@ -19,9 +19,12 @@ app.init = function () {
     url: app.yummlyApiUrl,
     method: 'GET',
     data: {
-      format: 'json',
+      format: 'jsonp',
       _app_id: app.yummlyApiID,
       _app_key: app.yummlyApiKey,
+      requirePictures: true,
+      maxResult: 100,
+      'allowedCuisine[]': 'cuisine^cuisine-chinese',
     }
   }).then(function (result) {
     console.log(result)
@@ -35,7 +38,7 @@ app.init = function () {
     dataType: 'json',
     data: {
       api_key: app.movieDBKey,
-      query: 'rocky',
+      // query: 'rocky',
       // sort_by: “popularity.desc”
     }
     }).then((res) => {
