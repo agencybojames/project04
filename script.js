660e931cfe27ff825f42f85abe3867d6552320e6
James <james83lau@gmail.com>
2019-02-21 21:08:44 -0500

    calling movie api

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const movieApp = {};
 
+movieApp.apiKey = '59be6234ea545ba637c135657e114e1d';
+movieApp.apiURL = 'https://api.themoviedb.org/3/movie/popular';
+
+movieApp.init= () => {
+movieApp.getMovies();
+}
+
+
+movieApp.getMovies = () => {
+    $.ajax({
+        url: movieApp.apiURL,
+        method:'GET',
+        dataType: 'jsonp',
+        data: {
+            key: movieApp.apiKey 
+        }
+    }).then((result) => {
+        console.log('test')
+    });
+}
