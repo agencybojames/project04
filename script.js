de696f282f4386ccba66802185419a512c6e20b0
Bojan <bojanpecanac89@gmail.com>
2019-02-24 13:53:16 -0500

    deleted yummly api

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const app = {};
 
-app.yummlyApiKey = '4232b467d64427cc7880acacaa0f27bc';
-app.yummlyApiUrl = 'http://api.yummly.com/v1/api/recipes?'; // http://api.yummly.com/v1/api/metadata/
-app.yummlyApiID = '44d38b57';
-
 app.movieDBKey = '59be6234ea545ba637c135657e114e1d'
 app.movieDBUrl = 'https://api.themoviedb.org/3/discover/movie'
 
@@ -12,22 +8,10 @@ $(function () {
 })
 
 app.formSubmit = function () {
-  // submit form and prevent default
+  // every time we 'submit', run the following code:
+  // prevent default
   $('form').on('submit', (event) => {
     event.preventDefault();
-
-    // every time we 'submit', run the following code:
-    // gather user choice of cuisine
-    app.userCuisine = $('#cuisineChoice').val()
-    console.log(app.userCuisine)
-
-    // gather user choice of flavour
-    app.userDiet = $('#dietChoice').val()
-    console.log(app.userDiet)
-
-    // gather user choice of cookTime
-    app.userCookTime = $('#timeChoice').val()
-    console.log(app.userCookTime)
 
     // gather user choice of Movie Genre
     app.userGenre = $('#genreChoice').val()
@@ -41,61 +25,40 @@ app.formSubmit = function () {
     app.userRating = $('#ratingChoice').val()
     console.log(app.userRating)
 
+    //call getData, which is a method that makes the API call.
     app.getData();
   })
 }
 
-
-const matchesArray = function(array){
-
-array.forEach(recipe => {
-  app.recipeId = recipe.id
-  // console.log(app.recipeId)
-})
-}
-
+//method which makes the ajax call and then returns
 app.getData = function () {
-  // AJAX CALL TO YUMMLY API
-  $.ajax({
-    url: app.yummlyApiUrl,
-    method: 'GET',
-    data: {
-      format: 'jsonp',
-      _app_id: app.yummlyApiID,
-      _app_key: app.yummlyApiKey,
-      requirePictures: true,
-      maxResult: 100,
-      'allowedCuisine[]': `cuisine^cuisine-${app.userCuisine}`,
-      'allowedDiet[]': app.userDiet,
-      'maxTotalTimeInSeconds': app.userCookTime
-    }
-  }).then(function (result) {
-    // console.log(result)
-    matchesArray(result.matches);
-
-  })
-
-  $.ajax({
-    url: `http://api.yummly.com/v1/api/recipe/${app.recipeId}?_app_id=${app.yummlyApiID}&_app_key=${app.yummlyApiKey}`,
-    method: 'GET'
-  }).then(function(yup) {
-    console.log(yup)
-  }) 
 
   // AJAX CALL TO MOVIE DB
-//   $.ajax({
-//     url: app.movieDBUrl,
-//     method: 'GET',
-//     dataType: 'json',
-//     data: {
-//       api_key: app.movieDBKey,
-//       // query: 'rocky',
-//       // sort_by: “popularity.desc”
-//     }
-//   }).then((res) => {
-//     // console.log(res.results)
-//   });
+  $.ajax({
+    url: app.movieDBUrl,
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      api_key: app.movieDBKey,
+      sort_by: 'popularity.desc',
+      'primary_release_date.gte': 1998,
+      'primary_release_date.lte': 2004,
+    }
+  }).then((res) => {
+    console.log(res.results)
+  });
 }
+
+
+
+
+
+
+
+
+
+
+
 
 app.init = function () {
 
